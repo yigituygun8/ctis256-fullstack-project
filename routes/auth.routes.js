882da78704fcea8f98c32e257580d0d970b337b6
Arda Planalı <ardaplnl.5@gmail.com>
2026-05-01 17:22:31 +0300

    Feature/authentication page (#2)
    * login page with sticky form and error checking
    
    * added necesarry classes for login page
    
    * post and get of /login endpoint structure is done with to do's
    
    * Added user_type selection with sticky form and error checking
    
    * i have created the register page
    
    * partials for the register page, input fields for customers and markets
    
    * deleted the user type selection radio button because it is not needed
    
    * implemented register endpoint with to do's

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,22 +1,58 @@
 import express from "express";
+import { body, validationResult } from "express-validator";
 const router = express.Router();
 
 // Login endpoints
 router.get('/login', (req, res) => {
-  // GET /login
+
+  res.render('login', { form : {}, errors: {}});
 });
 
-router.post('/login', (req, res) => {
-  // POST /login
+router.post('/login', 
+  body("email").notEmpty().withMessage("*This field must be filled"),
+  body("password").notEmpty().withMessage("*This field must be filled")
+  ,(req, res) => {
+
+
+  const errors = validationResult(req);
+
+  if(!errors.isEmpty()){
+    //Sended errors with .mapped() for easier checking
+    res.render('login', { form: req.body, errors : errors.mapped()});
+  } else{
+    //TO-DO: Email and Password check
+
+    res.redirect("/")
+  }
+
 });
 
 // Register endpoints
 router.get('/register', (req, res) => {
-  // GET /register
+  const user_type = req.query.type ?? 'cust';
+  res.render('register', { form : {}, errors: {}, user_type});
 });
 
-router.post('/register', (req, res) => {
-  // POST /register
+router.post('/register', 
+  body("email").notEmpty().withMessage("*This field must be filled"),
+  body("password").notEmpty().withMessage("*This field must be filled"),
+  body("city").notEmpty().withMessage("*This field must be filled"),
+  body("district").notEmpty().withMessage("*This field must be filled"),
+  body("fullName").if(body("user_type").equals("cust")).notEmpty().withMessage("*This field must be filled"),
+  body("marketName").if(body("user_type").equals("market")).notEmpty().withMessage("*This field must be filled")
+  ,(req, res) => {
+  
+  const errors = validationResult(req);
+  const user_type = req.body.user_type || req.query.type || 'cust';
+
+  if(!errors.isEmpty()){
+    //Sended errors with .mapped() for easier checking
+    res.render('register', { form: req.body, errors : errors.mapped(), user_type});
+  } else{
+    //TO-DO: Verification
+
+    res.redirect("/")
+  }
 });
 
 // Email verification endpoints
