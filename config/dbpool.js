52463d70271ade52795f712cc12f089a0e41830e
Osman Yiğit Uygun <osmanuygun197@gmail.com>
2026-05-01 15:27:08 +0300

    update config: root -> appuser

--- config/dbpool.js
+++ config/dbpool.js
@@ -3,7 +3,7 @@ import 'dotenv/config';
 
 export const pool = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
+    user: process.env.DB_USER || 'appuser',
     password: process.env.DB_PASSWORD || 'root',
     database: process.env.DB_NAME || 'test',
     port: Number(process.env.DB_PORT) || 3306,
