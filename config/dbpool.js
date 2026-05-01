835bc4448fdeb288bcfb4a972d6335ef0e474aab
Osman Yiğit Uygun <osmanuygun197@gmail.com>
2026-05-01 14:04:32 +0300

    updated docker compose file + config/dbpool.js with valid credentials

--- config/dbpool.js
+++ config/dbpool.js
@@ -3,8 +3,8 @@ import 'dotenv/config';
 
 export const pool = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'std',
-    password: process.env.DB_PASSWORD || '',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || 'root',
     database: process.env.DB_NAME || 'test',
     port: Number(process.env.DB_PORT) || 3306,
     waitForConnections: true,
