fe7f5de85e09e55ea8f90511f0cbaae5f8d7481c
Osman Yiğit Uygun <osmanuygun197@gmail.com>
2026-05-02 14:52:48 +0300

    feat: mounted routes and fixed issues

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -1,29 +1,26 @@
 import express from "express";
+import { addToCart, getCartData, removeFromCart, updateQuantity, completePurchase } from "../controllers/cartController.js";
 const router = express.Router();
 
 // View shopping cart
 router.get('/', (req, res) => {
-  // GET /cart - view shopping cart contents
+  // P.S. Initially, we decided that we could use a cart page but then we changed our mind and decided to use a cart sidebar instead. So, this endpoint is not used in the project but we left it here just in case we want to use it in the future.
+  res.redirect("/products");
 });
 
-// Add product to cart
-router.post('/add', (req, res) => {
-  // POST /cart/add - add product to cart
-});
+// Load cart data as JSON (for AJAX)
+router.get('/data', getCartData); 
 
-// Update product quantity in cart
-router.post('/update', (req, res) => {
-  // POST /cart/update - update product quantity in cart
-});
+// Add product to cart. Returns success status and message as JSON.
+router.post('/add', addToCart);
 
-// Remove product from cart
-router.post('/remove', (req, res) => {
-  // POST /cart/remove - remove product from cart
-});
+// Update product quantity in cart. Returns success status and message as JSON.
+router.post('/update', updateQuantity);
 
-// Complete purchase
-router.post('/purchase', (req, res) => {
-  // POST /cart/purchase - complete purchase
-});
+// Remove product from cart. Returns success status and message as JSON.
+router.post('/remove', removeFromCart);
+
+// Complete purchase. Returns success status and message as JSON.
+router.post('/purchase', completePurchase);
 
 export default router;
