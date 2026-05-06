50f0eb4099063d447675f6ed974c7a4ba4d8443c
BladeOfFreedom <ardaplnl.5@gmail.com>
2026-05-06 15:17:20 +0300

    implemented the GET edit end point for market products

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -2,7 +2,7 @@ import express from "express";
 const router = express.Router();
 import { pool } from "../config/dbpool.js";
 import upload from "../config/multer.js";
-import { getAllProducts, getProductDetails, searchProducts, getMarketDashboard } from "../controllers/productController.js";
+import { getAllProducts, getProductDetails, searchProducts, getMarketDashboard, getProduct } from "../controllers/productController.js";
 import { requireMarket } from "../middlewares/auth.middleware.js";
 
 // Get all products from any market (public)
@@ -32,11 +32,12 @@ router.post('/dashboard/product/new', upload.single('image'), (req, res) => {
 });
 
 // Market dashboard - form to edit existing product - render edit-product page with product details
-router.get('/dashboard/product/:id/edit', (req, res) => {
+router.get('/dashboard/product/:id/edit',  async (req, res) => {
   // GET /dashboard/product/:id/edit - form to edit existing product
-  const id = req.params.id ?? -1;
-  const product = getProduct(id);
-  res.render("productEdit", { product, user: req.session.user })
+  const { id }= req.params;
+  //console.log(id);
+  const product = await getProduct(id);
+  res.render("productEdit", { product: product, user: req.session.user })
 });
 
 // Market dashboard - update existing product
