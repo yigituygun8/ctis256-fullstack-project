3a6d600544904bd442efc7dea044926682a01b0e
Osman Yiğit Uygun <osmanuygun197@gmail.com>
2026-05-07 10:51:28 +0300

    fix: fixed multer config in destination

--- config/multer.js
+++ config/multer.js
@@ -4,7 +4,7 @@ import path from 'path';
 // Configure storage
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/products"); // Store relative path to the uploads directory in public folder
+    cb(null, "public/images"); // Store relative path to the uploads directory in public folder
   },
   filename: (req, file, cb) => {
     // Generate unique filename: timestamp-randomstring-originalname
